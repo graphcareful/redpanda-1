c412e5e6fae19bc36af0c137f47ff550cfc2be50
Rob Blafford <rob@vectorized.io>
2021-03-23 10:39:51 -0400

    rptest: Test a relatively large dataset with a wasm script

--- tests/rptest/tests/wasm_basic_test.py
+++ tests/rptest/tests/wasm_basic_test.py
@@ -20,23 +20,23 @@ class WasmBasicTest(WasmTest):
                         replication_factor=3,
                         cleanup_policy=TopicSpec.CLEANUP_DELETE), )
 
-    def __init__(self, test_context):
+    def __init__(self, test_context, num_records=1024, record_size=1024):
         super(WasmBasicTest, self).__init__(test_context, extra_rp_conf={})
+        self._num_records = num_records
+        self._record_size = record_size
+        assert len(self.topics) >= 1
 
     def wasm_test_plan(self):
-        input_topic = "myinputtopic"
+        input_topic = self.topics[0].name
         mapped_topic = "myoutputtopic"
         output_topic = construct_materialized_topic(input_topic, mapped_topic)
-
-        num_records = 1024
-        record_size = 1024
 
         # The identity transform produces 1 identital record onto a topic for
         # each input record. The result should be a 1 to 1 mapping between a
         # source and destination topic, they should be identical when compared
         basic_script = WasmScript(
-            inputs=[(input_topic, (num_records, record_size))],
-            outputs=[(output_topic, num_records)],
+            inputs=[(input_topic, (self._num_records, self._record_size))],
+            outputs=[(output_topic, self._num_records)],
             script=WasmTemplateRepository.IDENTITY_TRANSFORM)
 
         return [basic_script]
@@ -44,7 +44,23 @@ class WasmBasicTest(WasmTest):
     def assert_test_results(self, input_results, output_results):
         # Read all of the data from the materialized topic, expecting the same
         # number of records which were produced onto the input topic
-        assert input_results.num_records() == 1024
+        assert input_results.num_records() == self._num_records
         if input_results != output_results:
             raise Exception(
                 "Expected all records across topics to be equivalent")
+
+
+class WasmLargeDataSetTest(WasmBasicTest):
+    topics = (TopicSpec(name="biginputtopic",
+                        partition_count=3,
+                        replication_factor=3,
+                        cleanup_policy=TopicSpec.CLEANUP_DELETE), )
+
+    def __init__(self, test_context):
+        # 250,000 * 1024b ~ 1/4GB test
+        super(WasmLargeDataSetTest, self).__init__(test_context,
+                                                   num_records=250000,
+                                                   record_size=1024)
+
+    def wasm_test_timeout(self):
+        return (300, 3)
