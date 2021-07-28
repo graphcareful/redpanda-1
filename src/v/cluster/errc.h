7edf666e61d875381016eb6a73fecd37be085380
Rob Blafford <rob@vectorized.io>
2021-07-28 15:04:08 -0400

    cluster: Prevent deletion of materialized topics
    - Materialized topics cannot be explicity deleted by via the delete
    topics command.
    
    - Future PRs will support materialized topic deletion upon deletion of
    source topic(s).

--- src/v/cluster/errc.h
+++ src/v/cluster/errc.h
@@ -53,7 +53,7 @@ enum class errc : int16_t {
     partition_configuration_differs,
     data_policy_already_exists,
     data_policy_not_exists,
-
+    invalid_delete_topic_request
 };
 struct errc_category final : public std::error_category {
     const char* name() const noexcept final { return "cluster::errc"; }
@@ -144,6 +144,8 @@ struct errc_category final : public std::error_category {
             return "Data-policy already exists";
         case errc::data_policy_not_exists:
             return "Data-policy does not exist";
+        case errc::invalid_delete_topic_request:
+            return "Requested to delete a materialized topic is invalid";
         }
         return "cluster::errc::unknown";
     }
