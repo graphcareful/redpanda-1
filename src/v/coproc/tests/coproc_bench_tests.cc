4f346773829866257b650a4524ceff56b651c6c7
Rob Blafford <rob@vectorized.io>
2021-09-14 09:42:39 -0400

    coproc/tests: Use new API in coproc_bench*
    - ... and re-add it to the build system.
    
    - Was temporarily removed from the build system in an earilier commit to
    preserve bisectability and improve readability.
    
    - The coproc_bench_fixture was too complicated to begin with, its been
    simplified, but it was necessary to make some big changes as the test
    API has been modified.
    
    - The changes in the API meant that source ntps must be curried with
    materialized ones when creating the test plan.

--- src/v/coproc/tests/coproc_bench_tests.cc
+++ src/v/coproc/tests/coproc_bench_tests.cc
@@ -26,19 +26,20 @@ FIXTURE_TEST(test_coproc_router_multi_route, coproc_bench_fixture) {
     const model::topic input_one("one");
     const model::topic input_two("two");
     /// Expected topics coprocessors will produce onto
-    const model::topic even_mt_one = model::to_materialized_topic(
+    const model::topic even_mt_one = to_materialized_topic(
       input_one, two_way_split_copro::even);
-    const model::topic odd_mt_one = model::to_materialized_topic(
+    const model::topic odd_mt_one = to_materialized_topic(
       input_one, two_way_split_copro::odd);
-    const model::topic even_mt_two = model::to_materialized_topic(
+    const model::topic even_mt_two = to_materialized_topic(
       input_two, two_way_split_copro::even);
-    const model::topic odd_mt_two = model::to_materialized_topic(
+    const model::topic odd_mt_two = to_materialized_topic(
       input_two, two_way_split_copro::odd);
 
     /// Deploy coprocessors and prime v::storage with logs
     log_layout_map inputs = {{input_one, 1}, {input_two, 1}};
-    log_layout_map outputs = {
-      {even_mt_one, 1}, {odd_mt_one, 1}, {even_mt_two, 1}, {odd_mt_two, 1}};
+    copro_map mapping = {
+      {input_one, {even_mt_one, odd_mt_one}},
+      {input_two, {even_mt_two, odd_mt_two}}};
     setup(inputs).get();
 
     enable_coprocessors(
@@ -55,18 +56,16 @@ FIXTURE_TEST(test_coproc_router_multi_route, coproc_bench_fixture) {
       .get();
 
     /// Run the test
-    router_test_plan test_plan{
-      .input = build_simple_opts(inputs, 50),
-      .output = build_simple_opts(outputs, 25)};
-    auto result_tuple = start_benchmark(std::move(test_plan)).get0();
-    const auto& [push_results, drain_results] = result_tuple;
+    auto drain_results = start_benchmark(
+                           build_simple_opts(
+                             std::move(inputs), std::move(mapping), 50, 25))
+                           .get0();
 
     /// Expect all 4 partitions to exist and verify the exact number of
     // record  batches accross all
-    BOOST_REQUIRE_EQUAL(push_results.size(), 2);
     BOOST_REQUIRE_EQUAL(drain_results.size(), 4);
     for (const auto& [_, pair] : drain_results) {
-        const auto& [__, n_batches] = pair;
+        const auto& n_batches = pair;
         BOOST_REQUIRE_EQUAL(n_batches, 25);
     }
 }
@@ -75,7 +74,7 @@ FIXTURE_TEST(test_coproc_router_giant_fanin, coproc_bench_fixture) {
     const std::size_t n_copros = 50;
     const std::size_t n_partitions = 10;
     const model::topic source_topic("sole_input");
-    const model::topic output_topic = model::to_materialized_topic(
+    const model::topic output_topic = to_materialized_topic(
       source_topic, identity_coprocessor::identity_topic);
     std::vector<coproc_test_fixture::deploy> deploys;
     for (uint64_t i = 0; i < n_copros; ++i) {
@@ -87,21 +86,20 @@ FIXTURE_TEST(test_coproc_router_giant_fanin, coproc_bench_fixture) {
                source_topic, coproc::topic_ingestion_policy::stored)}}});
     }
     log_layout_map inputs = {{source_topic, n_partitions}};
-    log_layout_map outputs = {{output_topic, n_partitions}};
+    copro_map mapping = {{source_topic, {output_topic}}};
     setup(inputs).get();
     enable_coprocessors(std::move(deploys)).get();
 
-    router_test_plan test_plan{
-      .input = build_simple_opts(inputs, 10),
-      .output = build_simple_opts(outputs, 500)};
-    auto result_tuple = start_benchmark(std::move(test_plan)).get0();
-    const auto& [push_results, drain_results] = result_tuple;
+    auto drain_results = start_benchmark(
+                           build_simple_opts(
+                             std::move(inputs), std::move(mapping), 10, 500))
+                           .get0();
     const std::size_t n_record_batches = std::accumulate(
       drain_results.begin(),
       drain_results.end(),
       std::size_t(0),
       [](std::size_t acc, const auto& kv_pair) {
-          return acc += kv_pair.second.second;
+          return acc += kv_pair.second;
       });
     const std::size_t expected_record_batches = 10 * n_copros * n_partitions;
     BOOST_CHECK_EQUAL(n_record_batches, expected_record_batches);
@@ -121,26 +119,26 @@ FIXTURE_TEST(test_coproc_router_giant_one_to_many, coproc_bench_fixture) {
                source_topic, coproc::topic_ingestion_policy::stored)}}});
     }
     log_layout_map inputs = {{source_topic, n_partitions}};
-    log_layout_map outputs;
+    copro_map mapping = {{source_topic, {}}};
     for (std::size_t i = 0; i < n_copros; ++i) {
-        auto materialized_topic = model::to_materialized_topic(
+        auto materialized_topic = to_materialized_topic(
           source_topic, model::topic(ssx::sformat("identity_topic_{}", i)));
-        outputs.emplace(materialized_topic, n_partitions);
+        mapping.begin()->second.emplace_back(materialized_topic);
     }
     setup(inputs).get();
     enable_coprocessors(std::move(deploys)).get();
 
-    router_test_plan test_plan{
-      .input = build_simple_opts(inputs, 10),
-      .output = build_simple_opts(outputs, 10)};
-    auto result_tuple = start_benchmark(std::move(test_plan)).get0();
-    const auto& [push_results, drain_results] = result_tuple;
+    auto drain_results = start_benchmark(
+                           build_simple_opts(
+                             std::move(inputs), std::move(mapping), 10, 10))
+                           .get0();
+
     const std::size_t n_record_batches = std::accumulate(
       drain_results.begin(),
       drain_results.end(),
       std::size_t(0),
       [](std::size_t acc, const auto& kv_pair) {
-          return acc += kv_pair.second.second;
+          return acc += kv_pair.second;
       });
     const std::size_t expected_record_batches = 10 * n_copros * n_partitions;
     BOOST_CHECK_EQUAL(n_record_batches, expected_record_batches);
