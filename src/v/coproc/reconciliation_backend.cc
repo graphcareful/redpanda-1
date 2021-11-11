bc383d8423e1597a4e0153798fd27c31c25a16bb
Rob Blafford <rob@vectorized.io>
2021-11-11 10:19:47 -0500

    coproc: Reconciliation backend
    - The reconciliation backend will mimmic the
    cluster::controller_backend.
    
    - Its purpose is to handle all non_replicable* op type events that are
    emitted from the topics table.
    
    - The rational for this is that it is not possible for coproc to
    synchronize around the handling of these op_types if they are handled by
    code in cluster. It can only know that the event was emitted, not when
    it was finished.
    
    - With this solution the complexity of the existing
    cluster::controller_backend is reduced and more coproc code can stay in
    the coproc namespace

---
+++ src/v/coproc/reconciliation_backend.cc
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "coproc/reconciliation_backend.h"
+
+#include "cluster/cluster_utils.h"
+#include "cluster/shard_table.h"
+#include "cluster/topic_table.h"
+#include "coproc/logger.h"
+#include "coproc/pacemaker.h"
+#include "storage/api.h"
+
+#include <seastar/core/coroutine.hh>
+
+namespace coproc {
+
+reconciliation_backend::reconciliation_backend(
+  ss::sharded<cluster::topic_table>& topics,
+  ss::sharded<cluster::shard_table>& shard_table,
+  ss::sharded<storage::api>& storage) noexcept
+  : _self(model::node_id(config::node().node_id))
+  , _data_directory(config::node().data_directory().as_sstring())
+  , _topics(topics)
+  , _shard_table(shard_table)
+  , _storage(storage) {}
+
+ss::future<> reconciliation_backend::start() {
+    _id_cb = _topics.local().register_delta_notification(
+      [this](std::vector<update_t> deltas) {
+          for (auto& d : deltas) {
+              auto ntp = d.ntp;
+              _topic_deltas[ntp].push_back(std::move(d));
+          }
+          (void)ss::with_semaphore(
+            _sem, 1, [this]() { return fetch_and_reconcile(); });
+      });
+    return ss::now();
+}
+
+ss::future<> reconciliation_backend::stop() {
+    _as.request_abort();
+    _topics.local().unregister_delta_notification(_id_cb);
+    return _gate.close();
+}
+
+ss::future<> reconciliation_backend::fetch_and_reconcile() {
+    auto hold = _gate.hold();
+    if (_as.abort_requested()) {
+        co_return;
+    }
+    auto deltas = std::exchange(_topic_deltas, {});
+    co_await ss::parallel_for_each(
+      deltas.begin(),
+      deltas.end(),
+      [this](decltype(_topic_deltas)::value_type& deltas) -> ss::future<> {
+          for (auto& d : deltas.second) {
+              auto err = co_await process_update(std::move(d));
+              if (err != errc::success) {
+                  vlog(
+                    coproclog.warn,
+                    "Result {} returned after processing update {}",
+                    err,
+                    deltas.first);
+              }
+          }
+      });
+}
+
+ss::future<std::error_code> reconciliation_backend::process_update(update_t) {
+    /// Empty as this commit is just about setting up the framework that will
+    /// handle events. Commits that acutally handle the events are in subsequent
+    /// commits within this patch series
+    co_return errc::success;
+}
+
+} // namespace coproc
---
+++ src/v/coproc/reconciliation_backend.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+#include "cluster/fwd.h"
+#include "cluster/topic_table.h"
+#include "coproc/fwd.h"
+#include "storage/fwd.h"
+
+#include <seastar/core/gate.hh>
+#include <seastar/core/semaphore.hh>
+#include <seastar/core/sharded.hh>
+
+namespace coproc {
+
+class reconciliation_backend {
+public:
+    explicit reconciliation_backend(
+      ss::sharded<cluster::topic_table>&,
+      ss::sharded<cluster::shard_table>&,
+      ss::sharded<storage::api>&) noexcept;
+
+    /// Starts the reconciliation loop
+    ///
+    /// Listens for events emitted from the topics table
+    ss::future<> start();
+
+    /// Stops the reconciliation loop
+    ss::future<> stop();
+
+private:
+    using update_t = cluster::topic_table::delta;
+    ss::future<std::error_code> process_update(update_t);
+
+    ss::future<> fetch_and_reconcile();
+
+private:
+    cluster::notification_id_type _id_cb;
+    model::node_id _self;
+    ss::sstring _data_directory;
+    absl::flat_hash_map<model::ntp, std::vector<update_t>> _topic_deltas;
+
+    ss::gate _gate;
+    ss::abort_source _as;
+    ss::semaphore _sem{1};
+
+    ss::sharded<cluster::topic_table>& _topics;
+    ss::sharded<cluster::shard_table>& _shard_table;
+    ss::sharded<storage::api>& _storage;
+};
+
+} // namespace coproc
