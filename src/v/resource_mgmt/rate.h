4122350fa4d2ca247ed563ed43588b72f84061ba
Rob Blafford <rob@vectorized.io>
2022-04-13 14:24:13 -0400

    resource_mgmt: Add token bucket algorithm
    - Useful for rate-limiting solutions

--- src/v/resource_mgmt/rate.h
+++ src/v/resource_mgmt/rate.h
@@ -10,9 +10,11 @@
  */
 
 #pragma once
+#include "likely.h"
 #include "seastarx.h"
 
 #include <seastar/core/lowres_clock.hh>
+#include <seastar/util/bool_class.hh>
 
 #include <chrono>
 #include <numeric>
@@ -98,4 +100,84 @@ private:
     std::vector<window> _windows;
     clock::duration _window_size;
     std::size_t _current;
+};
+
+/// Token bucket algorithm, useful for rate-limiting solutions.
+///
+/// Keeps track of a limited number of resources known as 'tokens'.
+/// The current amount of tokens are kept in \ref bucket and when a user
+/// consumes resources they take a corresponding number of tokens from the
+/// bucket. Every \ref window duration the pre-configured amount of units are
+/// added to the bucket. If there are enough tokens in the bucket when queried
+/// the algorithm recommends rate-limiting, otherwise not.
+class token_bucket_rate_tracker {
+public:
+    using clock = ss::lowres_clock;
+    using units_t = int64_t;
+    using recommend_rate_limit
+      = ss::bool_class<struct recommend_rate_limit_tag>;
+
+    /// Class constructor
+    /// @param quota Expected rate to operate within
+    /// @param samples Number of windows to accumulate in memory for burst
+    /// @param window_sec width of window in which tokens are added - in seconds
+    token_bucket_rate_tracker(
+      uint32_t quota, uint32_t samples, clock::duration window_sec) noexcept
+      : _quota(quota)
+      , _samples(samples)
+      , _window(std::chrono::duration_cast<std::chrono::seconds>(window_sec))
+      , _last_check() {}
+
+    /// Slight variation of token bucket algorithm to allow bursty traffic
+    /// \ref burst tokens are removed from bucket after returning throttle
+    /// behavior suggestion
+    recommend_rate_limit
+    record_and_measure(uint32_t n, const clock::time_point& now) noexcept {
+        refill(now);
+        if (_bucket < 0) {
+            return recommend_rate_limit::yes;
+        }
+        _bucket = std::min<units_t>(burst(), _bucket - n);
+        return recommend_rate_limit::no;
+    }
+
+    void record(uint32_t n, const clock::time_point& now) noexcept {
+        refill(now);
+        if (_bucket >= 0) {
+            /// Number of tokens in bucket is allowed to become negative to
+            /// allow burst of traffic to proceed
+            _bucket = std::min<units_t>(burst(), _bucket - n);
+        }
+    }
+
+    units_t measure(const clock::time_point& now) noexcept {
+        refill(now);
+        return _bucket;
+    }
+
+    /// Returns the number of tokens that current remain in the bucket
+    units_t units() const noexcept { return _bucket; }
+
+private:
+    void refill(const clock::time_point& now_ms) noexcept {
+        const auto delta = std::chrono::duration_cast<std::chrono::seconds>(
+          now_ms - _last_check);
+        _last_check = now_ms;
+        _bucket = std::min<units_t>(burst(), _bucket + delta.count() * _quota);
+    }
+
+    uint32_t burst() const noexcept {
+        constexpr uint32_t max_val = std::numeric_limits<uint32_t>::max();
+        const auto b64 = static_cast<uint64_t>(_quota)
+                         * static_cast<uint64_t>(_samples)
+                         * static_cast<uint64_t>(_window.count());
+        return unlikely(b64 >= max_val) ? max_val : b64;
+    }
+
+private:
+    uint32_t _quota;
+    uint32_t _samples;
+    units_t _bucket{0};
+    std::chrono::seconds _window;
+    clock::time_point _last_check;
 };
