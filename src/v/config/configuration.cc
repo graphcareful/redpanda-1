1a790184d50b344c00a463d0c485f7faf67eb030
Rob Blafford <rob@vectorized.io>
2022-04-13 10:27:57 -0400

    config: Add partition_mutation_rate cfg property
    - Adding new configuration property that will be used by the newly
    added parititon mutation throttling functionality for allowing a
    configurable number of partition mutations per time unit

--- src/v/config/configuration.cc
+++ src/v/config/configuration.cc
@@ -229,6 +229,13 @@ configuration::configuration()
        .visibility = visibility::user},
       2_GiB,
       {.min = 1_MiB})
+  , partition_mutation_rate(
+      *this,
+      "partition_mutation_rate",
+      "Target quota rate (partition mutations per default_window_sec)",
+      {.needs_restart = needs_restart::no, .visibility = visibility::user},
+      std::nullopt,
+      {.min = 1})
   , cluster_id(
       *this,
       "cluster_id",
